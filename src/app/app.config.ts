3d88524681279a946d49d4ca9accd6fade288ebb
zzby <krisfromheaven@gmail.com>
2025-05-08 00:14:10 +0300

    eslint package.json

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,13 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideEventPlugins } from '@taiga-ui/event-plugins';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)],
+  providers: [
+    provideRouter(routes),
+    provideAnimations(),
+    provideEventPlugins(),
+  ],
 };
