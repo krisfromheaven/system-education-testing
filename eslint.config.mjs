ce196a3f89efe2e28ea71c792eb576e00cf54dad
zzby <krisfromheaven@gmail.com>
2025-05-08 16:31:33 +0300

    prepare setup && 403/404 pages

---
+++ eslint.config.mjs
@@ -0,0 +1,91 @@
+import { defineConfig, globalIgnores } from 'eslint/config';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default defineConfig([
+  globalIgnores([
+    'projects/**/*',
+    '**/node_modules/',
+    '**/dist/',
+    '**/.vscode',
+    '**/.idea',
+    '**/*.spec.ts',
+    '**/*.d.ts',
+  ]),
+  {
+    extends: compat.extends('plugin:prettier/recommended'),
+  },
+  {
+    files: ['**/*.ts'],
+
+    extends: compat.extends(
+      'plugin:@angular-eslint/recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:prettier/recommended',
+    ),
+
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+
+      parserOptions: {
+        project: ['tsconfig.json'],
+      },
+    },
+
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+        },
+      ],
+
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case',
+        },
+      ],
+
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase',
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/*.html'],
+    extends: compat.extends('plugin:@angular-eslint/template/recommended'),
+
+    rules: {
+      'max-len': [
+        'error',
+        {
+          code: 140,
+        },
+      ],
+
+      'prettier/prettier': 'error',
+    },
+  },
+]);
